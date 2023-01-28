8cc46583836ab9cdd69523e60ff11a66598ca874
Ayumu00 <61957287+Ayumu00@users.noreply.github.com>
2023-01-28 23:14:36 +0800

    Finished Product of my Game

--- script.js
+++ script.js
@@ -1,35 +1,80 @@
-function getComputerChoice(){
-    let choiceComputer = ["Rock","Paper","Scissors"];
-    choice = Math.floor(Math.random() * choiceComputer.length);
-    return choiceComputer[choice];
+function computerSelectGame(){
+    let lstItem = ["rock","paper","scissors"];
+    choice = Math.floor(Math.random()*lstItem.length);
+    return lstItem[choice];
 };
 
-function playRound(playerSelection, computerSelection){
-    let player = playerSelection;
-    let computer = computerSelection;
-    player = player.toLowerCase();
-
-    if ((player == "rock" && computer == "Paper") || (player == "scissors" && computer == "Rock") || (player == "paper" && computer == "Scissors")) {
-        user+=1;
-    } else if ((computer == "Rock" && player == "paper") || (computer == "Scissors" && player == "rock") || (computer == "Paper" && player == "scissors")) {
-        enemy+=1;
-    };
-  
+function playMechanics(player,enemy){
+  if (player == "rock" && enemy == "scissors"||player == "scissors" && enemy == "paper"||player == "paper" && enemy == "rock"){
+    playerS+=1;
+  } else if (enemy == "paper" && player == "rock"||enemy == "rock" && player == "scissors"||enemy == "scissors" && player == "paper"){
+    enemyS+=1;
+  } else if (player == "scissors" && enemy == "rock"||player == "paper" && enemy == "scissors"||player == "rock" && enemy == "paper"){
+    enemyS+=1;
+  } else if (enemy == "paper" && player == "scissors"||enemy == "rock" && player == "paper"||enemy == "scissors" && player == "rock"){
+    playerS+=1;
+  } else {
+    return;
+  }
 };
 
-function game(){
-    user = 0;
-    enemy = 0;
-
-    for (i=0;i<5;++i) {
-        playRound(prompt("Choose between Rock/Paper/Scissors"),getComputerChoice());
-    };
-    if(user > enemy){
-        return "You win!";
-    } else {
-        return "You lose!"
-    };
+function whoWin(){
+    if ((playerS > enemyS) && round >= 5){
+        playerS = 0, enemyS = 0, round = 0;
+        winLoseText.textContent = "WIN!"
+    } else if ((playerS < enemyS) && round >= 5){
+        playerS = 0, enemyS = 0, round = 0;
+        winLoseText.textContent = "LOSE!"
+    } else if ((playerS == enemyS) && round>=5){
+        round=0;
+        playerS=0;
+        enemyS=0;
+        winLoseText.textContent = "DRAW!"
+    }
 };
 
-let user = 0;
-let enemy = 0;
+function game(playerUser){
+    winLoseText.textContent = "";
+    round+=1;
+    let computerChoiceOnly = computerSelectGame();
+    playMechanics(playerUser,computerChoiceOnly);
+    playerScore.textContent = `${playerS}`;
+    enemyScore.textContent = `${enemyS}`;
+    roundNum.textContent = `${round}`;
+    let chooseA = playerUser;
+    let chooseB = computerChoiceOnly;
+    leftSide.src = `./images/${playerUser}.png`;
+    rightSide.src = `./images/${computerChoiceOnly}.png`;
+    whoWin();
+};
+
+function determine(e){
+    this.classList.value;
+    e.stopPropagation();
+    this.classList.add("effects-css");
+    let object = this.firstChild.src.match("rock")||this.firstChild.src.match("scissors")||this.firstChild.src.match("paper");
+    game(object[0]);
+};
+
+function removeTransition(e){
+    e.target.classList.remove("effects-css");
+};
+
+let playerS = 0;
+let enemyS = 0;
+let round = 0;
+
+// DOM
+
+const playerScore = document.querySelector(".player-score");
+const enemyScore = document.querySelector(".enemy-score");
+const roundNum = document.querySelector(".round-number");
+const leftSide = document.querySelector(".user");
+const rightSide = document.querySelector(".enemy");
+const Buttons = document.querySelectorAll("button");
+const winLoseText = document.querySelector(".win-lose-draw");
+
+Buttons.forEach(button => {
+    button.addEventListener("click",determine)
+    button.addEventListener("transitionend",removeTransition)
+});
